994f99d70b03cff64de7d4e7d487150788d41559
Mahdi Shabani <mahdishabani08@gmail.com>
2025-02-26 22:59:02 +0330

    dynamic route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,36 @@
 import express from "express";
 import path from "path";
+import fs from "fs";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(path.join(__dirname, "public/index.html"));
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/about.html"));
-});
-
+// Serve static files
 app.use("/styles", express.static(path.join(__dirname, "styles")));
 app.use("/scripts", express.static(path.join(__dirname, "scripts")));
 app.use("/components", express.static(path.join(__dirname, "components")));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Route for the home page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Dynamic routing for HTML pages
+app.get("/:page", (req, res) => {
+  const pageName = req.params.page;
+  const filePath = path.join(__dirname, `public/${pageName}.html`);
+  
+  // Check if the file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // File doesn't exist, send 404 page or redirect to home
+      return res.status(404).sendFile(path.join(__dirname, "public/404.html"));
+    }
+    
+    // File exists, send it
+    res.sendFile(filePath);
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
